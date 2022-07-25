31bc34ee480d027f41f130d676ab0b9217749398
Zelddd <96714947+Zelddd@users.noreply.github.com>
2022-07-25 18:52:05 +0800

    Update product.ts

--- routes/dashboard/kitchen/product.ts
+++ routes/dashboard/kitchen/product.ts
@@ -1,9 +1,33 @@
 import {Router} from 'express'
+import {Product} from 'models/product'
 
 const router = Router()
 
 router.get('/', async(req, res) => {
-    res.render('dashboard/kitchen/product', {title: 'Product'})
+    res.render('dashboard/kitchen/product', {
+        title: 'Product',
+        products: await Product.findAll(),
+    })
+})
+
+router.post('/', async (req, res) =>{
+    await Product.create({
+        // image: req.body.image, 
+        groupname: req.body.groupname, 
+        name: req.body.name,
+        price: req.body.price,   
+    })
+    res.redirect('/dashboard/kitchen/product') 
+})
+
+router.post('/:id', async (req, res) =>{
+    var product = await Product.findByPk(req.params.id)
+    await product.update({
+        groupname: req.body.groupname,
+        name: req.body.name,
+        price: req.body.price,
+    })
+    res.redirect('/dashboard/kitchen/product')
 })
 
 export default router;
