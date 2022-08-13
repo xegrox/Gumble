1c94f8e8755d30314e112744596b4dbd367aa414
Zelddd <96714947+Zelddd@users.noreply.github.com>
2022-08-14 01:58:25 +0800

    Update product.ts
    almost finished?

--- routes/dashboard/kitchen/product.ts
+++ routes/dashboard/kitchen/product.ts
@@ -1,5 +1,6 @@
 import {Router} from 'express'
 import {Product} from 'models/product'
+import {Category} from 'models/category'
 
 const router = Router()
 
@@ -7,32 +8,40 @@ router.get('/', async(req, res) => {
     res.render('dashboard/kitchen/product', {
         title: 'Product',
         products: await Product.findAll(),
+        categories: await Category.findAll(),
     })
+    console.log(await Category.findAll())
 })
 
 router.post('/', async (req, res) =>{
-    await Product.create({
+     await Product.create({
         image: req.body.image,
-        groupname: req.body.groupname, 
         name: req.body.name,
         price: req.body.price,   
     })
-    res.redirect('/dashboard/kitchen/product') 
+     res.redirect('/dashboard/kitchen/product') 
+}) 
+
+router.post('/category', async (req, res) =>{
+    await Category.create({
+        category: req.body.Category,
+    })
+    res.redirect('/dashboard/kitchen/product')
+    Category.findAll({ where: { category: '{{category}}' } })
 })
 
-router.post('/:id', async (req, res) =>{
+router.post('/product/update/:id', async (req, res) =>{
     var product = await Product.findByPk(req.params.id)
     await product.update({
         image: req.body.image,
-        groupname: req.body.groupname,
         name: req.body.name,
         price: req.body.price,
+        category: req.body.category,
     })
     res.redirect('/dashboard/kitchen/product')
 })
 
-router.post('/:id', async (req, res) =>{
-    await Category.create({
+export default router;
         category: req.body.category,
     })
     res.redirect('/dashboard/kitchen/product/category')
