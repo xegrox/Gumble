38f4d2546c3f9d2100031063118e04f5a38875d5
Zelddd <96714947+Zelddd@users.noreply.github.com>
2022-08-15 11:05:10 +0800

    Update product.ts

--- routes/dashboard/kitchen/product.ts
+++ routes/dashboard/kitchen/product.ts
@@ -7,17 +7,18 @@ const router = Router()
 router.get('/', async(req, res) => {
     res.render('dashboard/kitchen/product', {
         title: 'Product',
-        products: await Product.findAll(),
+        products: await Product.findAll({ where: { category_id: req.query.category ?? 1 } }),
         categories: await Category.findAll(),
     })
-    console.log(await Category.findAll())
+    console.log(await Product.findAll({raw: true}))
 })
 
 router.post('/', async (req, res) =>{
      await Product.create({
         image: req.body.image,
         name: req.body.name,
-        price: req.body.price,   
+        price: req.body.price,
+        category_id: req.body.category,   
     })
      res.redirect('/dashboard/kitchen/product') 
 }) 
@@ -36,8 +37,20 @@ router.post('/product/update/:id', async (req, res) =>{
         image: req.body.image,
         name: req.body.name,
         price: req.body.price,
-        category: req.body.category,
+        category_id: req.body.category,
     })
+    res.redirect('/dashboard/kitchen/product')
+})
+
+router.post('/delete/:id', async (req, res) =>{
+    var product = await Product.findByPk(req.params.id)
+    await product.destroy()
+    res.redirect('/dashboard/kitchen/product')
+})
+
+router.post('/category/delete/:id', async (req, res) =>{
+    var category = await Category.findByPk(req.params.id)
+    await category.destroy()
     res.redirect('/dashboard/kitchen/product')
 })
 
